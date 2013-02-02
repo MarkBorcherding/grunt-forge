7a276bdee39940d95b345401c3060b6bf56eb89b
Mark Borcherding <markborcherding@gmail.com>
2013-02-01 18:22:38 -0600

    refactor a bit

--- tasks/forge.js
+++ tasks/forge.js
@@ -17,31 +17,32 @@ module.exports = function(grunt) {
 
   var spawn = require('child_process').spawn;
 
-  // task
-  // ----
+  var puts = function(data) {
+    log.write(String(data));
+  };
 
 
   grunt.registerTask('forge:build', function(){
     var done = this.async();
     var forge = spawn("forge", ['build', 'ios']);
-    forge.stdout.on('data', function(data){ log.write("" + data); });
-    forge.stderr.on('data', function(data){ log.write("" + data); });
+    forge.stdout.on('data', puts);
+    forge.stderr.on('data', puts);
     forge.on('exit', function(data){ done(); });
   });
 
   grunt.registerTask('forge:sim', function(){
     var done = this.async();
     var forge = spawn("forge", ['run', 'ios']);
-    forge.stdout.on('data', function(data){ log.write("" + data); });
-    forge.stderr.on('data', function(data){ log.write("" + data); });
+    forge.stdout.on('data', puts);
+    forge.stderr.on('data', puts);
     forge.on('exit', function(data){ done(); });
   });
 
   grunt.registerTask('forge:device', function(){
     var done = this.async();
     var forge = spawn("forge", ['run', 'ios', '--io.device', 'device']);
-    forge.stdout.on('data', function(data){ log.write("" + data); });
-    forge.stderr.on('data', function(data){ log.write("" + data); });
+    forge.stdout.on('data', puts);
+    forge.stderr.on('data', puts);
     forge.on('exit', function(data){ done(); });
   });
 
