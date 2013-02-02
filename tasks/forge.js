e6e72be617387641e519bcd2425db34044f71706
Mark Borcherding <markborcherding@gmail.com>
2013-02-01 19:47:58 -0600

    updating grunt tasks

--- tasks/forge.js
+++ tasks/forge.js
@@ -2,31 +2,25 @@
 module.exports = function(grunt) {
   "use strict";
 
-  var log = grunt.log;
-  var verbose = grunt.verbose;
   var spawn = require('child_process').spawn;
 
   var puts = function(data) {
     log.write(String(data));
   };
 
-  var forge = function(options, done){
-    var cmd = spawn("forge", options);
+  grunt.registerMultiTask("forge", "Compile and run the app using forge.", function() {
+
+    var options = this.options({
+      command: "forge",
+    });
+
+    var  params = this.data.params;
+    var done = this.async();
+    var cmd = spawn(options.command, params);
     cmd.stdout.on('data', puts);
     cmd.stderr.on('data', puts);
     cmd.on('exit', function(data){ done(); });
-  };
 
-  grunt.registerTask('forge:build', function(){
-    forge(['build','ios'], this.async());
-  });
-
-  grunt.registerTask('forge:sim', function(){
-    forge(['run','ios'], this.async());
-  });
-
-  grunt.registerTask('forge:device', function(){
-    forge(['run', 'ios', '--io.device', 'device'], this.async());
   });
 
 };
